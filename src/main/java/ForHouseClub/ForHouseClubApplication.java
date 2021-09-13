0ce2f0c70906e826694148e02644ded47edd6b72
alvlap <alvlap@mail.ru>
2021-09-13 22:22:05 +0300

    Initial commit

---
+++ src/main/java/ForHouseClub/ForHouseClubApplication.java
@@ -0,0 +1,13 @@
+package ForHouseClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForHouseClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForHouseClubApplication.class, args);
+	}
+
+}
