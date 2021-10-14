f5a5c8e118a10f50ec4dddd8724eed726955b179
benefic <abenefic@gmail.com>
2021-10-14 22:46:44 +0300

    create estimate

--- src/main/java/club/forhouse/repositories/estimate/EstimateRepository.java
+++ src/main/java/club/forhouse/repositories/estimate/EstimateRepository.java
@@ -2,12 +2,13 @@ package club.forhouse.repositories.estimate;
 
 import club.forhouse.entities.profiles.Company;
 import club.forhouse.entities.estimate.Estimate;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface EstimateRepository extends JpaRepository<Estimate, Long> {
-    Optional<Estimate> findTopByCompany(Company company);
+    List<Estimate> findTop1ByCompany(Company company, Pageable pageable);
 }
