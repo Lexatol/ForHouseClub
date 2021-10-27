f45770439094cc08339ede191416aecf9c2d2eb7
alvlap <alvlap@mail.ru>
2021-10-27 10:06:25 +0300

    update delete update main.html update users.html update tenderView.html update profileCompanies.html update sql files

--- src/main/java/club/forhouse/controllers/tenders/TenderController.java
+++ src/main/java/club/forhouse/controllers/tenders/TenderController.java
@@ -1,16 +1,10 @@
 package club.forhouse.controllers.tenders;
 
-import ch.qos.logback.core.status.Status;
 import club.forhouse.dto.tenders.SystemTenderDto;
-import club.forhouse.dto.tenders.TCompanyDto;
 import club.forhouse.dto.tenders.TenderDto;
 import club.forhouse.entities.profiles.Company;
-import club.forhouse.entities.tenders.StatusTender;
-import club.forhouse.entities.tenders.Tender;
 import club.forhouse.exceptions.MarketError;
 import club.forhouse.services.profiles.CompanyService;
-import club.forhouse.services.tenders.StatusTenderService;
-import club.forhouse.services.tenders.TenderPlatformService;
 import club.forhouse.services.tenders.TendersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,9 +18,7 @@ import java.util.List;
 @RequestMapping("api/v1/tenders")
 public class TenderController {
     private final TendersService tendersService;
-    private  final TenderPlatformService tenderPlatformService;
     private final CompanyService companyService;
-    private final StatusTenderService statusTenderService;
 
     @GetMapping("id/{id}")
     public TenderDto findById(@PathVariable Long id) {
@@ -60,46 +52,8 @@ public class TenderController {
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-    @GetMapping("/set_contractor/{c}/{id}")
-    public void setCompanyContractor(@PathVariable String c, @PathVariable Long id) {
-        Company company = companyService.findByName(c);
-        Tender tender = tendersService.findTenderById(id);
-        TCompanyDto tCompanyDto = new TCompanyDto();
-        tCompanyDto.setCompanyName(c);
-        tender.setContractor(company);
-        StatusTender status = statusTenderService.findByTitle("ждет подтверждения");
-        tender.setStatus(status);
-        tendersService.saveOrUpdate(tender);
-    }
-
-    @GetMapping("/remove_contractor/{id}")
-    public void removeCompanyContractor(@PathVariable Long id) {
-        Tender tender = tendersService.findTenderById(id);
-        tender.setContractor(null);
-        StatusTender status = statusTenderService.findByTitle("объявлен тендер");
-        tender.setStatus(status);
-        tendersService.saveOrUpdate(tender);
-    }
-
-    @GetMapping("/approve_contractor/{id}")
-    public void approveCompanyContractor(@PathVariable Long id) {
-        Tender tender = tendersService.findTenderById(id);
-        StatusTender status = statusTenderService.findByTitle("в работе");
-        tender.setStatus(status);
-        tendersService.saveOrUpdate(tender);
-    }
-
-    @GetMapping("/close_tender/{id}")
-    public void closeTender(@PathVariable Long id) {
-        Tender tender = tendersService.findTenderById(id);
-        StatusTender status = statusTenderService.findByTitle("тендер завершен");
-        tender.setStatus(status);
-        tendersService.saveOrUpdate(tender);
-    }
-
-    @GetMapping("/remove/{id}")
-    public void removeTender(@PathVariable Long id){
-        Tender tender = tendersService.findTenderById(id);
-        tendersService.deleteThis(tender);
+    @DeleteMapping
+    public void delete(@RequestParam Long tenderId) {
+        tendersService.delete(tenderId);
     }
 }
